b5d43d70f7f8b14ca1a916669af8c1ae5d157bba
Raheem0299 <raheem0299@gmail.com>
2024-02-01 19:35:53 -0600

    CRUD + Employees
    conectado a SQL y crud funcionando

---
+++ ModuloLimitadorVelocidad/Data/MVC_DbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModuloLimitadorVelocidad.Models.Domain;
+
+namespace ModuloLimitadorVelocidad.Data
+{
+    public class MVC_DbContext : DbContext
+    {
+        public MVC_DbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ ModuloLimitadorVelocidad/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ModuloLimitadorVelocidad.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
