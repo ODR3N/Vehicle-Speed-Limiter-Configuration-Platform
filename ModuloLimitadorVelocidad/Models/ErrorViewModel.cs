d705a25e18755991ac9c3663eaac1b4e48809c19
DiegolCR <106277922+DiegolCR@users.noreply.github.com>
2024-02-22 20:24:41 -0600

    Delete ModuloLimitadorVelocidad directory

--- ModuloLimitadorVelocidad/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ModuloLimitadorVelocidad.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
