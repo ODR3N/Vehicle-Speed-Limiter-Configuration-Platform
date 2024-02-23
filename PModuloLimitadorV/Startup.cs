dbe960249022a8d6b1c13cf7a13eacb4e0e1d596
DiegolCR <diego200305@gmail.com>
2024-02-22 20:18:59 -0600

    Cambio De Modelo de Trabajo a ASP.NET.Aplication

---
+++ PModuloLimitadorV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PModuloLimitadorV.Startup))]
+namespace PModuloLimitadorV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ PModuloLimitadorVelocidad/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PModuloLimitadorVelocidad.MvcApplication" Language="C#" %>
