c38ab8d2e8b35e9a504093067f98638d06ff42d0
ODR3N <84418893+ODR3N@users.noreply.github.com>
2024-02-27 19:45:37 -0600

    Creacion de Tab de Configuracion del Modulo y funcion de reconocimiento automatico del puerto COM donde esta conectado el modulo.

---
+++ PModuloLimitadorV/Controllers/ModuleController.cs
@@ -0,0 +1,46 @@
+﻿using PModuloLimitadorV.Models;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace PModuloLimitadorV.Controllers
+{
+    public class ModuleController : Controller
+    {
+        // GET: Module
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ModuleRead()
+        {
+           
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult ObtenerPuertosDisponibles()
+        {
+            var puertosDisponibles = SerialPort.GetPortNames();
+
+            if (puertosDisponibles.Length > 0)
+            {
+                return Json(new { ConexionExitosa = true, Puerto = puertosDisponibles[0] }, JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new { ConexionExitosa = false, Mensaje = "No se ha detectado el módulo" }, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
+    }
+
+}
---
+++ PModuloLimitadorV/Models/Vehiculo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace PModuloLimitadorV.Models
+{
+    public class Vehiculo
+    {
+        public int ID { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; }
+    }
+}
